2fe64b8220306d4e99e9377e65969a727134d520
jn64 <23169302+jn64@users.noreply.github.com>
2023-06-17 02:25:51 +0800

    Fix typo

--- .gollum/config.rb
+++ .gollum/config.rb
@@ -10,7 +10,7 @@ Precious::App.set(:wiki_options, wiki_options)
 Precious::App.set(:default_markup, :markdown)
 
 # Enable footnotes, smart punctuation (e.g. curly quotes), and all extensions
-# as of commonmark 0.23.9.
+# as of commonmarker 0.23.9.
 # See <https://github.com/gjtorikian/commonmarker/blob/42cfc90251353f9fceda91b884d0ded8d3da0bcf/README.md#options>
 GitHub::Markup::Markdown::MARKDOWN_GEMS["commonmarker"] = proc { |content, options: {}|
   CommonMarker.render_html(content, [:FOOTNOTES, :SMART], [:tagfilter, :autolink, :table, :strikethrough, :tasklist])
